c6fc2c1313506594d916c36bc926cb50a5f2b49e
alexdonega <alexdonega@gmail.com>
2026-08-31 13:57:54 -0300

    feat: add webinar landing page at /webinar

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import App from './App.jsx'
 import GrupoVip from './GrupoVip.jsx'
+import Webinar from './Webinar.jsx'
 import Login from './Login.jsx'
 import Leads from './Leads.jsx'
 import LeadDetail from './LeadDetail.jsx'
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/mega-oferta" element={<GrupoVip />} />
+                <Route path="/webinar" element={<Webinar />} />
                 <Route path="/grupo-vip" element={<Navigate to="/mega-oferta" replace />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/lead" element={<Leads />} />
